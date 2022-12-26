195d2113eaa5aa6dd76d283d513ee33a294c811c
Ruben Westerberg <drclaw@mac.com>
2022-12-26 19:44:00 +1000

    Added acceptor

---
+++ lib/uSAC/IO/AE/Acceptor.pm
@@ -0,0 +1,64 @@
+use Object::Pad;
+
+package uSAC::IO::AE::Acceptor;
+
+#Only force a nonblocking setting if needed. ie linux => true
+#     bsd and dawrwin false
+#     others true
+use constant SET_NONBLOCKING=>$^O eq "darwin"? undef: $^Oeq "linux"?1:1;
+
+use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
+use AnyEvent;
+
+use IO::FD;
+
+class uSAC::IO::AE::Acceptor :isa(uSAC::IO::Acceptor);
+
+use feature qw<refaliasing current_sub say>;
+no warnings qw<experimental uninitialized>;
+field $_aw;
+field $_acceptor;
+		
+field $_afh_ref;
+
+BUILD {
+	$_afh_ref=\$self->fh;
+}
+
+method start :override ($fh=undef){
+	$$_afh_ref=$fh if $fh;
+	$_aw= AE::io $$_afh_ref, 0, $_acceptor//=$self->_make_acceptor;
+	$self;
+}
+
+method _make_acceptor :override {
+
+  \my $on_accept=\$self->on_accept; #alias the accept callback
+  \my $on_error=\$self->on_error; #alias the error callback
+  $_aw=undef; #Ensure watcher is dead
+  \my $afh=$_afh_ref;
+  my @new; my $new=\@new;
+  my @peers; my $peers=\@peers;
+
+  #Return a sub which is used in the AE::io call
+  sub {
+    my $res= IO::FD::accept_multiple @new, @peers, $afh;
+    if(defined $res){ 
+      if(SET_NONBLOCKING){ 
+        IO::FD::fcntl $_, F_SETFL, O_NONBLOCK for @new;
+      }
+      #execute the callback with the array refs
+      $on_accept->($new, $peers);
+    }
+    else {
+      $on_error->($!);
+    }
+  }
+}
+
+method pause :override {
+  #Destroy the IO watcher
+  undef $_aw;
+}
+
+1;
---
+++ lib/uSAC/IO/Acceptor.pm
@@ -0,0 +1,55 @@
+use Object::Pad;
+package uSAC::IO::Acceptor;
+use uSAC::IO::Common;
+use IO::FD;
+
+use feature qw<refaliasing current_sub say>;
+no warnings qw<experimental uninitialized>;
+use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
+
+
+class uSAC::IO::Acceptor;
+
+
+
+
+field $_fh :param :mutator;
+field $_on_accept :param :mutator;
+field $_on_error :param :mutator;
+		
+	
+BUILD{
+	$_fh=fileno $_fh if ref($_fh);	#Ensure we are working with a fd
+	die "Could not set NON BLOCKING mode for fd" unless defined IO::FD::fcntl $_fh, F_SETFL, O_NONBLOCK;
+
+
+	$_on_accept//=sub {$self->pause};
+	$_on_error//=sub{$self->pause};
+
+}
+
+
+method start {
+	#method class for backend to override
+}
+
+method _make_acceptor {
+
+}
+
+method pause{
+	#method class for backend to override
+}
+
+
+my $backend=uSAC::IO::Common::detect_backend;
+
+my $rb=($backend."::Acceptor");
+eval "require $rb";
+
+#Wrapper 
+sub create { shift; $rb->new(@_); }
+1;
+
+__END__
+
