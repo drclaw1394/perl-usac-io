d7002fe373a9cc8067b50d81c065666dea83016e
Ruben Westerberg <drclaw@mac.com>
2024-07-08 09:53:08 +1000

    Working _prep_spec
    socket_stage now uses _prep_spec for generating hints/specs for sockets
    
    This is asyncrhonous

--- lib/uSAC/IO.pm
+++ lib/uSAC/IO.pm
@@ -10,6 +10,9 @@ our $VERSION="v0.1.0";
 #Datagram
 use Import::These qw<uSAC::IO:: DReader DWriter SWriter SReader>;
 
+use Import::These qw<Socket::More:: Constants Interface>;
+use constant::more  IPV4_ANY=>"0.0.0.0",
+                    IPV6_ANY=>"::";
 
 use Socket::More;
 use Socket::More::Resolver {}, undef;
@@ -17,6 +20,7 @@ use IO::FD::DWIM ();
 use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK :mode);
 
 use Data::Dumper;
+use Data::Cmp qw<cmp_data>;
 
 
 
@@ -123,21 +127,39 @@ sub _create_socket {
 # Create sockets based on specs
 #  spec, and callbacks
 #
-sub socket_stage ($$){
+sub socket_stage($$){
+  say "asdf";
   my ($spec, $next)=@_;
-
+  my $copy;
   if(!ref $spec){
     # Assume string which needs parsing
-    $spec=parse_passive_spec $spec;
+    $copy=parse_passive_spec $spec;
+  }
+  else {
+    # copy
+    %$copy=%$spec;
   }
 
   #TODO merge spec with merge items
   
-  # Generate a list of hints from the spec 
-  my @res=sockaddr_passive $spec;
+  # Override an undefined on_spec function to create a socket
+  my $on_spec=$copy->{data}{on_spec}//sub { 
+    _create_socket undef, $_[1], $next if $_[1];
+  };
 
-  # Start of the stages by creating a socket and calling on_socket
-  _create_socket undef, $_, $next for(@res);
+  _prep_spec($copy, $on_spec);
+
+  1;
+
+  ####################################################################
+  # # Generate a list of hints from the spec                         #
+  # say Dumper $spec;                                                #
+  # my @res=sockaddr_passive $spec;                                  #
+  # say "sdfasdf";                                                   #
+  # say Dumper @res;                                                 #
+  # # Start of the stages by creating a socket and calling on_socket #
+  # _create_socket undef, $_, $next for(@res);                       #
+  ####################################################################
 }
 
 
@@ -241,7 +263,7 @@ sub connect ($$){
 		$ok=Socket::More::Resolver::getaddrinfo(
 			$host,
 			$port,
-      undef, #$hints,
+      $hints,
       sub {
         say STDERR "LOOKUP callback"; 
         my @addresses=@_;
@@ -252,6 +274,7 @@ sub connect ($$){
 
       sub{
         say STDERR "LOOKUP ERROR"; 
+        say STDERR Dumper $hints;
         $on_error and $on_error->($socket, gai_strerror $!);
       }
 		);
@@ -309,6 +332,344 @@ sub accept($$){
   $a->start;
   
 }
+
+
+
+# Asynchronous version of  sockaddr_passive
+#
+sub _prep_spec{
+	require Scalar::Util;
+	my ($spec, $on_spec)=@_;
+
+  $on_spec//=$spec->{data}{on_spec};
+  my $on_error=$spec->{data}{on_error};
+
+  # v0.5.2 Copy the input specs
+  my %copy=%$spec;
+  $spec=\%copy;
+
+  ## Filter
+	my @seen;
+
+
+	my $r={};
+
+	#If no interface provided assume all
+	$r->{interface}=$spec->{interface}//".*";
+	
+  
+	$r->{socktype}=$spec->{socktype}//[SOCK_STREAM, SOCK_DGRAM];
+	$r->{protocol}=$spec->{protocol}//0;
+
+	#If no family provided assume all
+	$r->{family}=$spec->{family}//[AF_INET, AF_INET6, AF_UNIX];	
+	
+	#Configure port and path
+	$r->{port}=$spec->{port}//[];
+	$r->{path}=$spec->{path}//[];
+	
+  
+  # Convert to arrays for unified interface 
+  for($r->{socktype}, $r->{family}){
+    unless(ref eq "ARRAY"){
+      $_=[$_];
+    }
+  }
+
+  for($r->{socktype}->@*){
+    unless(Scalar::Util::looks_like_number $_){
+      ($_)=string_to_socktype $_;
+    }
+  }
+
+  for($r->{family}->@*){
+    unless(Scalar::Util::looks_like_number $_){
+      ($_)=string_to_family $_;
+    }
+  }
+  # End
+  #####
+
+
+	#NOTE: Need to add an undef value to port and path arrays. Port and path are
+	#mutually exclusive
+	if(ref($r->{port}) eq "ARRAY"){
+		unshift $r->{port}->@*, undef;
+	}
+	else {
+		$r->{port}=[undef, $r->{port}];
+	}
+
+
+	if(ref($r->{path}) eq "ARRAY"){
+		unshift $r->{path}->@*, undef;
+	}
+	else {
+		$r->{path}=[undef, $r->{path}];
+	}
+
+	die "No port number specified, no address information will be returned" if ($r->{port}->@*==0) or ($r->{path}->@*==0);
+
+	#Delete from combination specification... no need to make more combos
+  #
+  my $enable_group=exists $spec->{group};
+
+	my $address=delete $spec->{address};
+	my $group=delete $spec->{group};
+	my $data=delete $spec->{data};
+  my $flags=(delete $spec->{flags})//0;
+
+	$address//=".*";
+	$group//=".*";
+
+	#Ensure we have an array for later on
+	if(ref($address) ne "ARRAY"){
+		$address=[$address];
+	}
+
+	if(ref($group) ne "ARRAY"){
+		$group=[$group];
+	}
+
+	my @interfaces=(Socket::More::make_unix_interface, Socket::More::getifaddrs);
+
+	#Check for special cases here and adjust accordingly
+	my @new_address;
+	my @new_interfaces;
+	##my @new_spec_int;
+	my @new_fam;
+
+  # IF IPV4_ANY or IPV6_ANY is specified,  nuke any other address provided
+  #
+	if(grep /${\IPV4_ANY()}/, @$address){
+		#push @new_spec_int, IPV4_ANY;
+		push @new_address, IPV4_ANY;
+		push @new_fam, AF_INET;
+    my @results;
+    Socket::More::Lookup::getaddrinfo(
+      IPV4_ANY,
+      "0",
+      {flags=>AI_NUMERICHOST|AI_NUMERICSERV, family=>AF_INET},
+      @results
+    );
+
+
+		push @new_interfaces, ({name=>IPV4_ANY,addr=>$results[0]{addr}});
+	}
+
+	if(grep /${\IPV6_ANY()}/, @$address){
+		#push @new_spec_int, IPV6_ANY;
+		push @new_address, IPV6_ANY;
+    push @new_fam, AF_INET6;
+    my @results;
+    Socket::More::Lookup::getaddrinfo(
+      IPV6_ANY,
+      "0",
+      {flags=>AI_NUMERICHOST|AI_NUMERICSERV, family=>AF_INET6},
+      @results
+    );
+
+    push @new_interfaces, ({name=>IPV6_ANY, addr=>$results[0]{addr}});
+	}
+
+
+  # TODO: Also add special case for multicast interfaces? for datagrams?
+
+	if(@new_address){
+		@$address=@new_address;
+		@interfaces=@new_interfaces;
+		$r->{interface}=[".*"];
+	}
+
+	#$r->{family}=[@new_fam];
+
+	#Handle localhost
+	if(grep /localhost/, @$address){
+		@$address=('^127.0.0.1$','^::1$');
+		$r->{interface}=[".*"];
+	}
+
+  
+
+  $r->{address}=$address;
+
+	#Generate combinations
+	my $result=Data::Combination::combinations $r;
+	
+
+	#Retrieve the interfaces from the os
+	#@interfaces=(make_unix_interface, Socket::More::getifaddrs);
+
+
+	#Poor man dereferencing
+	my @results=$result->@*;
+	
+	#Force preselection of matching interfaces
+  #say "Interfaces before ", Dumper @interfaces;
+	@interfaces=grep {
+		my $interface=$_;
+		scalar grep {$interface->{name} =~ $_->{interface}} @results
+	} @interfaces;
+  #say "Interfaces after ", Dumper @interfaces;
+
+	#Validate Family and fill out port and path
+  no warnings "uninitialized";
+
+	my @output;
+
+  #Total number of probable combinations
+  my $count=@interfaces*@results;
+
+	for my $interface (@interfaces){
+		my $fam= sockaddr_family($interface->{addr});
+		for(@results){
+
+			next if $fam != $_->{family};
+
+			#Filter out any families which are not what we asked for straight up
+
+			goto CLONE if ($fam == AF_UNIX) 
+				&& ($interface->{name} eq "unix")
+				#&& ("unix"=~ $_->{interface})
+				&& (defined($_->{path}))
+				&& (!defined($_->{port}));
+
+
+			goto CLONE if
+				($fam == AF_INET or $fam ==AF_INET6)
+				&& defined($_->{port})
+				&& !defined($_->{path})
+				&& ($_->{interface} ne "unix");
+
+			next;
+
+	CLONE:
+			my %clone=$_->%*;			
+			my $clone=\%clone;
+			$clone{data}=$spec->{data};
+      $clone{flags}=$spec->{flags};
+
+			#A this point we have a valid family  and port/path combo
+			#
+			my ($err, $res, $service);
+
+      # Complete the clone interface info
+      $clone->{interface}=$interface->{name};
+      $clone->{if}=$interface;  # From v0.5.0
+
+			#copy data to clone
+			$clone->{data}=$data;
+      $clone->{flags}=$flags;
+
+
+      if($fam == AF_UNIX){
+        # Assume no lookup is needed for this
+        my $suffix=$_->{socktype}==SOCK_STREAM?"_S":"_D";
+				$clone->{addr}=pack_sockaddr_un $_->{path}.$suffix;
+				my $path=unpack_sockaddr_un($clone->{addr});			
+				$clone->{address}=$path;
+				$clone->{path}=$path;
+				$clone->{interface}=$interface->{name};
+				$clone->{group}="UNIX" if $enable_group;
+
+      }
+
+      elsif(!exists $_->{address} or $_->{address} eq ".*"){
+        # No address to look up, assuming the binary addr field is set
+        #
+        say  "Address needs to be filled";
+        if($fam == AF_INET){
+          my (undef, $ip)=unpack_sockaddr_in($interface->{addr});
+          Socket::More::Lookup::getnameinfo($interface->{addr}, my $host="", my $port="", NI_NUMERICHOST|NI_NUMERICSERV);
+
+          $clone->{address}=$host;
+          $clone->{addr}=pack_sockaddr_in($_->{port}, $ip);
+          if($enable_group){
+            require Socket::More::IPRanges;
+            $clone->{group}=Socket::More::IPRanges::ipv4_group($clone->{address});
+          }
+        }
+
+        elsif($fam == AF_INET6){
+          my(undef, $ip, $scope, $flow_info)=unpack_sockaddr_in6($interface->{addr});
+          Socket::More::Lookup::getnameinfo($interface->{addr}, my $host="", my $port="", NI_NUMERICHOST|NI_NUMERICSERV);
+          $clone->{address}=$host;
+          $clone->{addr}=pack_sockaddr_in6($_->{port},$ip, $scope, $flow_info);
+          if($enable_group){
+            require Socket::More::IPRanges;
+            $clone->{group}=Socket::More::IPRanges::ipv6_group($clone->{address});
+          }
+        }
+        else {
+          # Unsupported AF
+        }
+
+        # break synchronous callback
+        next unless grep {$clone->{address}=~ /$_/i } @$address;
+        if($enable_group){
+          next  unless grep {$clone->{group}=~ /$_/i } @$group;
+        }
+        next unless defined $clone->{addr};
+        
+        
+        #send out for async
+        my $found;
+        for(my $i=0; $i<@seen; $i++){
+          my $s=$seen[$i];
+          $found=grep {!cmp_data($clone, $s)} @seen; 
+          last if $found;
+        }
+
+        if(!$found){
+          push @seen, $clone;
+          $on_spec and asap $on_spec, undef, $clone;
+        }
+      }
+
+      else {
+        # Address exists so we (potentially need to) lookup to generate binary addr field
+        #
+          my @results;
+          Socket::More::Lookup::getaddrinfo($_->{address},$_->{port},$_, @results);
+          $clone->{addr}=$results[0]{addr};
+
+          Socket::More::Resolver::getaddrinfo($_->{address},$_->{port},$_, 
+            sub {
+              # NOTE ONLY USES THE FIRST RESULT
+              $clone->{addr}=$_[0]{addr};
+              return unless grep {$clone->{address}=~ /$_/i } @$address;
+              if($enable_group){
+                return unless grep {$clone->{group}=~ /$_/i } @$group;
+              }
+              return unless defined $clone->{addr};
+              
+              #send out for async
+              my $found;
+              for(my $i=0; $i<@seen; $i++){
+                my $s=$seen[$i];
+                my $found=grep {!cmp_data($clone, $s)} @seen; 
+                last if $found;
+              }
+
+              if(!$found){
+                push @seen, $clone;
+                $on_spec and $on_spec->(undef, $clone);
+              }
+
+            },
+
+            $on_error    # Use on error
+          );
+      }
+    }
+  }
+
+  # Send end message
+  #$on_spec and asap $on_spec, undef, undef;
+}
+
+
+
 
 
 sub dreader {
