204d77b95c1de75e456f1021b40aa17516d7316f
Ruben Westerberg <drclaw@mac.com>
2025-06-10 14:30:25 +1000

    Fix callback on error
    General callbacks are not called on errors
    
    TODO clean up queuing elements

--- lib/uSAC/IO/AE/SWriter.pm
+++ lib/uSAC/IO/AE/SWriter.pm
@@ -7,14 +7,13 @@ no warnings qw<experimental uninitialized>;
 use AnyEvent;
 use uSAC::Log;
 use Log::OK;
-#use IO::FD::DWIM ":all";
 use IO::FD;
 
 use Errno qw(EAGAIN EINTR);
 use parent "uSAC::IO::Writer";
 use uSAC::IO::Writer qw<:fields>;
 
-use constant::more RECUSITION_LIMIT=>5;
+use constant::more RECUSITION_LIMIT=>10;
 use constant::more DEBUG=>0;
 
 field $_ww;		# Actual new variable for sub class
@@ -67,7 +66,6 @@ method _make_writer :override {
         \my $offset=\$entry->[1];
         \my $cb=\$entry->[2];
 
-        #\my $arg=\$entry->[3];
         $time=$clock;
         #$offset+=$w = IO::FD::syswrite4 $wfh, $buf, length($buf)-$offset, $offset;
         $offset+=$w = $syswrite->( $wfh, $buf, length($buf)-$offset, $offset);
@@ -80,6 +78,9 @@ method _make_writer :override {
           }
           #$e->[2]($e->[3]) if $e->[2];
           &{$e->[2]} if $e->[2];
+          $e->[2]=undef if $e->[2];
+          @$e=();
+          $buf=undef;
         }
         elsif(!defined($w) and $! != EAGAIN and $! != EINTR){
           #this is actual error
@@ -89,7 +90,7 @@ method _make_writer :override {
           $wfh=undef;
           @queue=();	#reset queue for session reuse
           $on_error and $on_error->($!);
-          $cb and $cb->();
+          #$cb and $cb->();
         }
       };
 
@@ -110,6 +111,8 @@ method _make_writer :override {
     #Push to queue if watcher is active or need to do a async call
     if($_recursion_counter > RECUSITION_LIMIT or defined $_ww){
       push @queue, [$_[0][0], 0, $cb];
+      $_[0][0]=undef;
+      $cb=undef;
       #Watcher or queue active to ensure its running.
       ($_ww = AE::io($wfh, 1, $sub)) unless ($_ww and $wfh);
       return();
@@ -124,8 +127,12 @@ method _make_writer :override {
     $w = $syswrite->($wfh, $_[0][0]);
 
     if( $w==length($_[0][0]) ){
+      #DEBUG and Log::OK::TRACE and log_trace unpack "H*",$_[0][0] if $w<100;
       DEBUG and Log::OK::TRACE and log_trace "SWriter DID write all.. doing callback  length $w";
+      DEBUG and Log::OK::TRACE and log_trace "QUEUE length is: @queue";
+      $_[0][0]=undef;
       $cb and &$cb;
+      $cb=undef;
     }
     elsif(!defined($w) and $! != EAGAIN and $! != EINTR){
       #this is actual error
@@ -134,13 +141,18 @@ method _make_writer :override {
       $_ww=undef;
       $wfh=undef;
       @queue=();	#reset queue for session reuse
-      $cb and $cb->();
+
+      #$_[0][0]=undef;
+      #$cb and $cb->();
+      $cb=undef;
       $on_error and $on_error->($!);
     }
     else {
       #The write did not send all the data. Queue it for async writing
       DEBUG and Log::OK::TRACE and log_trace "SWriter could not write all.. adding to queue";
       push @queue,[$_[0][0], $w, $cb];
+      $cb=undef;
+      $_[0][0]=undef;
       $_ww = AE::io $wfh, 1, $sub unless $_ww;
     }
     return ();
@@ -154,6 +166,10 @@ method _make_reseter {
       DEBUG and Log::OK::TRACE and log_trace "SIO: SWRITE reset stack called";
       $_recursion_counter=0;
       $_ww=undef;
+      for(@queue){
+        #$_->[0]=undef
+        $_->[2]=undef;
+      }
       @queue=();
   }
 
