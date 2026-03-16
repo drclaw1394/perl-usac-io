d2bb7326aa7f3a4e46e87e8abb94a4d2cfdabae2
Ruben Westerberg <drclaw@mac.com>
2026-03-17 09:58:05 +1000

    Added io.pl

---
+++ expermiments/io.pl
@@ -0,0 +1,27 @@
+use uSAC::IO;
+
+use Data::Dumper;
+use Devel::Mat::Dumper;
+use feature ":all";
+
+sub error {
+  adump $STDERR, "Got error ", @_;
+}
+
+my $fid;
+my $counter=100000;
+
+my $head;
+$head= linker io_file_slurp(\&error), sub {
+  say "Counter: ",$counter--; 
+  
+  unless($counter){
+    Devel::MAT::Dumper::dump("pmay.dat");
+    exit;
+  }
+  #say STDERR "fid: $fid,,,,DISPATCH";
+  #say STDERR Dumper @_; 
+  asap sub {$head->( [{path=>"test.txt", mode=>"<"}], undef)};
+};
+
+$head->( [{path=>"test.txt", mode=>"<"}], undef);
